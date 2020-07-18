ee4a7896eb2165f4e0d3e4a21fecce61e3d2344c
Punyasloka Parida <12557052+twoCansOfTomatoes@users.noreply.github.com>
2020-07-19 01:30:39 +0530

    Modified Vue default page

--- pages/index.vue
+++ pages/index.vue
@@ -1,29 +1,8 @@
 <template>
   <div class="container">
-    <div>
-      <Logo />
-      <h1 class="title">
-        punyasloka-space
+        <h1 class="title">
+       Punyasloka
       </h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
-    </div>
   </div>
 </template>
 
@@ -43,7 +22,8 @@ export default {}
 
 .title {
   font-family:
-    'Quicksand',
+  'Rockwell',
+    'Quicksand',    
     'Source Sans Pro',
     -apple-system,
     BlinkMacSystemFont,
